4a7e702324122a28b7480e1c333d594bd1c6938b
Jasonnmss <greatxgaming@Gmail.com>
2026-05-09 07:57:09 +0200

    implemented bullet to bullet collision and shit ton of fixes

--- game-jam-2026/dna.gd
+++ game-jam-2026/dna.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends Area2D
 
 var speed = -400
 
@@ -12,3 +12,13 @@ func _physics_process(delta: float) -> void:
 	life_spawn += delta
 	if life_spawn > life_time:
 		queue_free()
+		
+func _on_body_entered(body):
+	if body.has_method("damage"):                
+		body.damage()
+		queue_free()
+		
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("bullet"):
+			queue_free()
+			
